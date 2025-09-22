b2dd6cc4e2a81b8c6b5bfd4f6fd04b4d99d98c81
ikarosbernardini <ikaros.bernardini@gmail.com>
2025-09-22 14:20:43 +0200

    ändrade lite

--- Elin.py
+++ Elin.py
@@ -1,1 +1,39 @@
-print ("Hello World!") # Elin är bäst
+# Vanligversion
+test = True
+if test == True:
+    tal = 34
+else:
+    tal = 0
+
+
+# Snabbversion av if 
+tal = 34 if test==True else 0 
+
+
+# Vanligversion
+for x in range(10):
+    lista = x 
+
+
+# Snabbversion av for 
+lista =  [x for x in range(10)] 
+
+def addera(tal1, tal2):
+    return tal1 + tal2
+
+def returnera(tal1, tal2):
+    return tal1, tal2
+
+värden = returnera(32, 43)
+
+värde1,värd2 = returnera(32, 43)
+
+for i,value in enumerate(["A","B","C","D"]):
+    print(f"index={str(i)} och value = {value}")
+
+for value in {"A":"1","B":2,"C": "text"}.values():
+    pass 
+for key in {"A":"1","B":2,"C": "text"}.keys():
+    pass 
+for key,value in {"A":"1","B":2,"C": "text"}.items():
+    pass 

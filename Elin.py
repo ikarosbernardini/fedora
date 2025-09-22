b1f6b7abbcab9c906286a4af881e442e872ead3a
ikarosbernardini <ikaros.bernardini@gmail.com>
2025-09-22 14:41:17 +0200

    test

--- Elin.py
+++ Elin.py
@@ -31,9 +31,26 @@ värde1,värd2 = returnera(32, 43)
 for i,value in enumerate(["A","B","C","D"]):
     print(f"index={str(i)} och value = {value}")
 
+    #v1,v2 = tal1, tal2, 
+
 for value in {"A":"1","B":2,"C": "text"}.values():
     pass 
 for key in {"A":"1","B":2,"C": "text"}.keys():
     pass 
 for key,value in {"A":"1","B":2,"C": "text"}.items():
     pass 
+
+
+
+Lista = [1,2,3,4,5,6,3,4]
+Dict = {1:"Hello",2:"World",3:"!"}
+Tuple = (42, 23) = 42, 23  
+
+set(Lista) # tar borta alla dubbletter och läser enbart in unika värden. bra för att sortera ut objekt som vi ej vill ha dubbelt av. 
+
+
+# Decorator, kör annan kod innnan och/eller efter er kod .
+@test()
+def addera(tal1, tal2):
+    pass 
+

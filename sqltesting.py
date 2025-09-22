955981de6e64134ab7cabfaea6865c1a102272a9
ikarosbernardini <ikaros.bernardini@gmail.com>
2025-09-22 15:48:12 +0200

    t

--- sqltesting.py
+++ sqltesting.py
@@ -2,7 +2,7 @@ import sqlite3
 con = sqlite3.connect("tutorial.db") # här skapas en databasfil
 cur = con.cursor()  # em cursor används för att utföra SQL-kommandon
 
-# cur.execute("CREATE TABLE IF NOT EXISTS movie(title,year,score)") # här skapas en databas-tabell
+#cur.execute("CREATE TABLE IF NOT EXISTS movie(title,year,score)") # här skapas en databas-tabell
 
 res = cur.execute("SELECT title FROM movie")
 print(res.fetchone())
@@ -10,4 +10,35 @@ print(res.fetchone())
 res = cur.execute("SELECT title FROM movie WHERE title='spam'")
 print(res.fetchall())
 
+#cur.execute("""
+   # INSERT INTO movie VALUES
+       # ('Monty Python and the Holy Grail', 1975, 8.2),
+        #('And Now for Something Completely Different', 1971, 7.5)
+#""") 
 
+con.commit()
+
+res = cur.execute("SELECT score FROM movie")
+print(res.fetchall())
+
+
+data = [("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+    ("Monty Python's The Meaning of Life", 1983, 7.5),
+    ("Monty Python's Life of Brian", 1979, 8.0),
+]
+
+#cur.executemany("INSERT INTO movie VALUES (?,?,?)", data)
+con.commit()
+
+
+for row in cur.execute("SELECT year, title FROM movie ORDER BY year"):
+    print(row)
+
+con.close()
+new_con = sqlite3.connect("tutorial.db")
+new_cur = new_con.cursor()
+res = new_cur.execute("SELECT title, year FROM movie ORDER BY score DESC")
+title, year = res.fetchone()
+print(f'The highest scoring Monty Python movie is {title!r}, released in {year}')
+
+new_con.close()
